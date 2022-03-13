415715a44b21e35ec17e96631431468ee683f861
Grigorev Pavel <grigorevpv@gmail.com>
2022-03-13 22:34:38 +0200

    Base setup eslint for typescript project

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import cron from 'node-cron';
-import { Telegram } from 'telegraf';
+import cron from 'node-cron'
+import { Telegram } from 'telegraf'
 
-const telegram: Telegram = new Telegram(process.env.BOT_TOKEN as string);
+const telegram: Telegram = new Telegram(process.env.BOT_TOKEN as string)
 
-cron.schedule("15 15 * * 2,4", () => {
+cron.schedule('15 15 * * 2,4', () => {
     telegram.sendMessage(
         process.env.CHAT_ID as string,
         'Привет! Я буду иногда напоминать полить нашего друга, а именно пн/чт в 17:00. Большое спасибо, что проявляешь заботу о нём, я это очень ценю. Хорошего тебе вечера ;)'
-    );
-});
+    )
+})
