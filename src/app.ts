9434d88f0246c5b90969596549831cc7d55a34fb
Grigorev Pavel <grigorevpv@gmail.com>
2022-03-14 23:25:56 +0200

    Update cron schedule

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { Telegram } from 'telegraf'
 
 const telegram: Telegram = new Telegram(process.env.BOT_TOKEN as string)
 
-cron.schedule('15 15 * * 2,4', () => {
+cron.schedule('0 15 * * 1,4', () => {
     telegram.sendMessage(
         process.env.CHAT_ID as string,
         'Привет! Я буду иногда напоминать полить нашего друга, а именно пн/чт в 17:00. Большое спасибо, что проявляешь заботу о нём, я это очень ценю. Хорошего тебе вечера ;)'
