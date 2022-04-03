9fc96b9794f9e5d32fd55388050add76d3101a40
Grigorev Pavel <grigorevpv@gmail.com>
2022-04-03 14:58:24 +0300

    Update send animation function

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import cron from 'node-cron'
 import { Telegram, Telegraf } from 'telegraf'
 
-import { botSignature, daylyTasksCount, scheduledReminderMessage } from './consts'
+import { botSignature, catAnimationUrl, daylyTasksCount, scheduledReminderMessage } from './consts'
 import { createDaylyCronDateFrom9To18, createDaylyCronTasks } from './services/scheduler'
 import { getRequest, HttpRequest } from './transport/httpRequest'
 
@@ -17,26 +17,15 @@ cron.schedule('0 9 * * 2,5', () => {
 })
 
 createDaylyCronTasks(daylyTasksCount, createDaylyCronDateFrom9To18, async () => {
-    const catAnimation = await getRequest(req, 'cataas.com', '/cat/gif')
-
     telegram.sendAnimation(
         process.env.CHAT_ID as string,
-        catAnimation
+        { url: catAnimationUrl }
     )
 })
 
 bot.on('message', async (ctx, next) => {
     try {
         const id = Number(process.env.TRACKED_MEMBER_ID)
-
-        if (ctx.update.message.from.id === 200484588) {
-            const catAnimation = await getRequest(req, 'cataas.com', '/cat/gif')
-
-            telegram.sendAnimation(
-                process.env.CHAT_ID as string,
-                catAnimation
-            )
-        }
 
         if (ctx.update.message.from.id === id) {
             const { compliment } = await getRequest(req, 'complimentr.com', '/api')
--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,9 @@
+// String values
 export const botSignature = 'Your BacBo bot'
 export const scheduledReminderMessage = 'Привет! Я буду иногда напоминать полить нашего друга, а именно вт/пт в 11:00. Большое спасибо, что проявляешь заботу о нём, я это очень ценю. Хорошего тебе продолжения дня ;)'
+
+// Number values
 export const daylyTasksCount = 3
+
+// url values
+export const catAnimationUrl = 'https://cataas.com/cat/gif'
